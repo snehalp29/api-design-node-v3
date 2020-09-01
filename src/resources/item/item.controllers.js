b3c94d4a1a146253a7b27c98a411825b6b54f51e
snehalp29 <snehalpatkar@gmail.com>
2020-09-01 11:52:32 -0400

    added routes

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,1 +1,9 @@
 export default {}
+
+export const getItem = (req, res) => {
+  res.status(200).json({ data: req.item })
+}
+
+export const postItem = (req, res) => {
+  res.status(200).json({ data: req.item })
+}
